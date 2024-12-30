cdc17909c3daed95c59f9d73d25f3601aa5d6f6b
mi-stas <mi-stas@users.noreply.github.com>
2024-12-30 17:02:56 +0300

    update generated file

--- task_1/func_1.cpp
+++ task_1/func_1.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 int f(void) {
-	int var1 = 2;
-	float var2{17};
-	double var3 = 52;
-	char var4{9};
+    int var1 = 2;
+    float var2{17};
+    double var3 = 52;
+    char var4{9};
     std::cout << var1 + var2 - var3 / var4;
-	return 0;
+    return 0;
 }
 
 int main() {
-	f();
+    f();
 }
